9f4ce9cb34bc79cc4c0fef1a38a399eaf77f12a3
tharlikar <tharlikar@gmail.com>
2022-04-26 02:46:14 +0630

    init migration from AspNetCore2.2 to AspNetCore6.0

---
+++ EmployeeManagement-20220425T144508Z-001/EmployeeManagement/EmployeeManagement/Program.cs
@@ -0,0 +1,122 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContextPool<AppDbContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDBConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 10;
+    options.Password.RequiredUniqueChars = 3;
+
+    options.SignIn.RequireConfirmedEmail = true;
+
+    options.Tokens.EmailConfirmationTokenProvider = "CustomEmailConfirmation";
+
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders()
+.AddTokenProvider<CustomEmailConfirmationTokenProvider
+    <ApplicationUser>>("CustomEmailConfirmation");
+
+builder.Services.Configure<DataProtectionTokenProviderOptions>(o =>
+            o.TokenLifespan = TimeSpan.FromHours(5));
+
+builder.Services.Configure<CustomEmailConfirmationTokenProviderOptions>(o =>
+            o.TokenLifespan = TimeSpan.FromDays(3));
+
+builder.Services.AddMvc(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+    options.EnableEndpointRouting = false;
+}).AddXmlSerializerFormatters();
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = "443892072779-3n0ljac2jnar4kmnnlkn74h4ic1tdq54.apps.googleusercontent.com";
+        options.ClientSecret = "7C6TvX2SWEodUuXd3EpsoO1R";
+    })
+    .AddFacebook(options =>
+    {
+        options.AppId = "2316662895109472";
+        options.AppSecret = "e25c1b8d4145034ed426d7a05efe1481";
+    });
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = new PathString("/Administration/AccessDenied");
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("DeleteRolePolicy",
+        policy => policy.RequireClaim("Delete Role"));
+
+    options.AddPolicy("EditRolePolicy",
+        policy => policy.AddRequirements(new ManageAdminRolesAndClaimsRequirement()));
+
+    options.AddPolicy("AdminRolePolicy",
+        policy => policy.RequireRole("Admin"));
+});
+
+builder.Services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+
+builder.Services.AddSingleton<IAuthorizationHandler, CanEditOnlyOtherAdminRolesAndClaimsHandler>();
+builder.Services.AddSingleton<IAuthorizationHandler, SuperAdminHandler>();
+builder.Services.AddSingleton<DataProtectionPurposeStrings>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.UseMvc(routes =>
+//{
+//    routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+//});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
